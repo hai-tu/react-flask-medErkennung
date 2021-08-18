d7001b9a07f2a1825b7ed874d85fbeef00c7060c
hai-tu <hai-tu@utulsa.edu>
2021-08-18 23:19:25 +0200

    first page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {Container, Button, Box, Typography} from '@material-ui/core'
-import { AppBar, Toolbar, IconButton} from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Grid, Paper} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
-import { RepeatOneSharp } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: '0 0 1em 0',
-    width: '40%',
+    width: '50%',
     height:'40px',
   },
   menuButton: {
     marginRight: theme.spacing(2),
+  },
+  forGrid: {
+    margin: '0.5em 0 0 0',
+    height: 150,
+  },
+  gridItem: {
+    margin: '0.5em 0 0 0',
+    width: '500px',
+    height: 300,
+  },
+  paper: {
+    height: 140,
+    width: 200,
   },
   title: {
     flexGrow: 1,
@@ -21,9 +33,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function App() {
-  const videoRef = useRef()
-  const photoRef = useRef()
+  const videoRef = useRef();
+  const photoRef = useRef();
   const classes = useStyles();
+  const imageRef = useRef();
 
   // useEffect(() => {
   //   fetch('/time').then(res => res.json()).then(data => {
@@ -60,20 +73,24 @@ function App() {
     photoRef.current.height = videoHeight;
     
     context.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
+    
+    photoRef.current.toBlob((blob) => {
+      imageRef.current = blob;
+    })
   }
 
   async function detect() {
-    if (photoRef.current) {
+    if (imageRef.current) {
       const formData = new FormData();
-      formData.append('image', photoRef.current);
+      formData.append('image', imageRef.current);
       const response = await fetch('/detect', {
         method: "POST",
         body: formData,
       })
-
+      
       if (response.status === 200) {
-        const text = await response.text();
-        alert("done");
+        const text = await response.json()
+        console.log(text)
       } else {
         alert("somtthing wrong");
       }
@@ -93,12 +110,49 @@ function App() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <Box bgcolor="lightblue">
+      <Box bgcolor="lightblue" display="flex" flexDirection="row">
         <div className="Live-Section" >
           <video className="streaming" ref={videoRef} onCanPlay={() => playCameraStream()} id="video" />
           <Button className={classes.button} variant="contained" color="primary" onClick={takePhoto}>Take Picture</Button>
           <Button className={classes.button} variant="contained" color="primary" onClick={detect}>Detect</Button>
           <canvas className="streaming" ref={photoRef} />
+        </div>
+        <div className="Result" >
+          <Grid container className={classes.forGrid} justifyContent="center" spacing={5}>
+              {[0, 1].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+          </Grid>
+          <Grid container className={classes.forGrid} justifyContent="center" spacing={5}>
+              {[0, 1].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+          </Grid>
+          <Grid container className={classes.forGrid} justifyContent="center" spacing={5}>
+              {[0, 1].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+          </Grid>
+          <Grid container className={classes.forGrid} justifyContent="center" spacing={5}>
+              {[0, 1].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+          </Grid>
+          <Grid container className={classes.forGrid} justifyContent="center" spacing={5}>
+              {[0, 1].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+          </Grid>
         </div>
       </Box>
     </div>
